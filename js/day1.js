9b678b0972e91313271ec2cadc572441b8f59660
zayid mohamed <sayidka843@gmail.com>
2022-07-13 22:18:40 +0600

    added files

--- js/day1.js
+++ js/day1.js
@@ -3,5 +3,8 @@ let h1 = document.querySelector("h1");
 
 function changeColor() {
   let usr = prompt("what you change :");
+  if (usr == "menu") {
+    menu.classList;
+  }
 }
 console.log(changeColor());
